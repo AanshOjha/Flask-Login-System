19d419be2c8a94a452c9356ecc576bbaa9f5cad2
AanshOjha <1aanshojha@gmail.com>
2024-04-18 00:28:47 +0530

    From new device

--- envconfig.py
+++ envconfig.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
+# Load environment variables from .env file!!!
 load_dotenv()
 
 # Access environment variables
