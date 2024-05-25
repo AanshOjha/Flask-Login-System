c80c1cee259abff9d228c1ae3c7ec4301f7f3ec3
AanshOjha <1aanshojha@gmail.com>
2024-05-25 11:26:39 +0530

    Used K8S

--- flaskalbum/__init__.py
+++ flaskalbum/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
 import os
-# from dotenv import load_dotenv
-# load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
